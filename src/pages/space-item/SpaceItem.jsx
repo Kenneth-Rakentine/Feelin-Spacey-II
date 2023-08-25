91eb35f9981c5a1fd0cf5d95d2e831bf16755865
Kenny Rakentine <specialistmorgenj@gmail.com>
2023-08-25 09:50:29 -0400

    placeholder alt for broken img

--- src/pages/space-item/SpaceItem.jsx
+++ src/pages/space-item/SpaceItem.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
+
+
 const SpaceItem = ( { item } ) => {
   return (
     <div className='itemContainer'>
       <h1>ENTER SPACE</h1>
       <h2>{item.title}</h2>
-      <img src={item.url} alt='nasa img' />
+      <img src={item.url} alt='https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/800px-No-Image-Placeholder.svg.png'/>
 
       <p>{item.explanation}</p>
 
-
+      <button onClick={handleGoBack}>Go Back</button>
     </div>
   )
 }
